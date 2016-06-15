ebe502ca04636bc8d33ec69b67ef1529ae970bf2
Kira <kiraclimber@gmail.com>
2016-06-15 14:56:50 -0600

    Forgot to add this file. Resizing image

--- ImageSplit.java
+++ ImageSplit.java
@@ -10,21 +10,25 @@ public class ImageSplit {
     private int chunks;
     private String[] files;
     private Random gen;
+    private int screenHeight;
 	
 	
-	public ImageSplit(int r, int c){
+	public ImageSplit(int r, int c, int h){
 		rows = r;
 		cols = c;
 		chunks = rows*cols;
 		files = new String[]{"src/kittySized.png", "src/kittyReach.png", "src/look.png", "src/yellowKitty.png","src/poo.png", "src/sadKitty.png","src/happyKitty.png",
 				"src/cuteKitty.png","src/doubleKitty.png", "src/kiki.png","src/helloKitty.png"};//random pictures
 		gen=new Random();
+		screenHeight = h;
 	}
 	
 	public BufferedImage[][] chop() throws IOException{
 		File file = new File(files[gen.nextInt(files.length)]); 
 	    FileInputStream fis = new FileInputStream(file);
 	    BufferedImage image = ImageIO.read(fis); //reading the image file
+	    image.getScaledInstance(screenHeight, screenHeight, Image.SCALE_DEFAULT);
+
 
 	    
 
